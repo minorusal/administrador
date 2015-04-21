8192538fc3e0470b114161b2810b841d920dad6a
jmartinez <jorge.martinez@isolution.mx>
2015-04-21 15:40:41 -0500

    correcion

--- application/models/inventario/catalogos_model.php
+++ application/models/inventario/catalogos_model.php
@@ -1,8 +1,6 @@
 <?php
 class catalogos_model extends Base_Model{
-	
-	/*Articulos*/
-	public function filtrar_articulos($data){
+	function filtrar_articulos($data){
 		$query = "SELECT 
 					ca.id_cat_articulo
 					,ca.articulo
@@ -24,7 +22,8 @@ class catalogos_model extends Base_Model{
 			return $query->result_array();
 		}
 	}
-	public function detalle_articulos($data){
+
+	function detalle_articulos($data){
 		$query = "SELECT 
 					ca.id_cat_articulo
 					,ca.articulo
@@ -42,7 +41,8 @@ class catalogos_model extends Base_Model{
 			return $query->result_array();
 		}
 	}
-	public function get_articulos($limit, $offset){
+
+	function get_articulos($limit, $offset){
 		$query = "SELECT 
 					ca.id_cat_articulo
 					,ca.articulo
@@ -59,6 +59,7 @@ class catalogos_model extends Base_Model{
 			return $query->result_array();
 		}
 	}
+	
 	public function get_total_articulos(){
 		$query = "SELECT 
 					*
@@ -68,6 +69,7 @@ class catalogos_model extends Base_Model{
        	$query = $this->db->query($query);
         return $query->num_rows();
     }
+
 	public function insert_articulos($data){
 		$existe = $this->row_exist('av_cat_articulos', array('clave_corta'=> $data['clave_corta']));
 		if(!$existe){
@@ -79,104 +81,13 @@ class catalogos_model extends Base_Model{
 			return false;
 		}
 	}
+
 	public function update_articulos($data, $id_usuario){
 		$condicion = array('id_cat_articulo !=' => $id_usuario, 'clave_corta = '=> $data['clave_corta']); 
 		$existe = $this->row_exist('av_cat_articulos', $condicion );
 		if(!$existe){
 			$condicion = "id_cat_articulo = $id_usuario"; 
 			$query = $this->db->update_string('av_cat_articulos', $data, $condicion);
-			$query = $this->db->query($query);
-			return $query;
-		}else{
-			return false;
-		}
-	}
-
-
-	/*Lineas*/
-	public function filtrar_lineas($data){
-		$query = "SELECT 
-					c.id_cat_linea
-					,c.linea
-					,c.clave_corta
-					,c.descripcion
-				FROM
-					av_cat_lineas c
-				WHERE 
-					c.activo = 1
-				AND (
-					c.linea like '%$data%'
-				OR 
-					c.clave_corta like '%$data%'
-				OR
-					c.descripcion like '%$data%')";
-
-       $query = $this->db->query($query);
-		if($query->num_rows >= 1){
-			return $query->result_array();
-		}
-	}
-	public function detalle_lineas($data){
-		$query = "SELECT 
-					c.id_cat_linea
-					,c.linea
-					,c.clave_corta
-					,c.descripcion
-					,c.timestamp
-					,c.id_usuario
-				FROM
-					av_cat_lineas c
-				WHERE 
-					c.id_cat_linea = $data";
-
-       $query = $this->db->query($query);
-		if($query->num_rows >= 1){
-			return $query->result_array();
-		}
-	}
-	public function get_lineas($limit, $offset){
-		$query = "SELECT 
-					c.id_cat_linea
-					,c.linea
-					,c.clave_corta
-					,c.descripcion
-				FROM
-					av_cat_lineas c
-				WHERE c.activo = 1
-
-				LIMIT $offset ,$limit";
-       
-       $query = $this->db->query($query);
-		if($query->num_rows >= 1){
-			return $query->result_array();
-		}
-	}
-	public function get_total_lineas(){
-		$query = "SELECT 
-					*
-				FROM
-					av_cat_lineas c
-				WHERE c.activo = 1";
-       	$query = $this->db->query($query);
-        return $query->num_rows();
-    }
-	public function insert_lineas($data){
-		$existe = $this->row_exist('av_cat_lineas', array('clave_corta'=> $data['clave_corta']));
-		if(!$existe){
-			$query = $this->db->insert_string('av_cat_lineas', $data);
-			$query = $this->db->query($query);
-
-			return $query;
-		}else{
-			return false;
-		}
-	}
-	public function update_lineas($data, $id_usuario){
-		$condicion = array('id_cat_linea !=' => $id_usuario, 'clave_corta = '=> $data['clave_corta']); 
-		$existe = $this->row_exist('av_cat_lineas', $condicion );
-		if(!$existe){
-			$condicion = "id_cat_linea = $id_usuario"; 
-			$query = $this->db->update_string('av_cat_lineas', $data, $condicion);
 			$query = $this->db->query($query);
 			return $query;
 		}else{

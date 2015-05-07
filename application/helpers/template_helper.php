c985182a01cbcb825b86f93a013839714fdd6e2a
jmartinez <jorge.martinez@isolution.mx>
2015-05-07 12:16:22 -0500

    creacion de vista proveedores save

--- application/helpers/template_helper.php
+++ application/helpers/template_helper.php
@@ -117,7 +117,7 @@
 				$count++;
 			}
 
-			$selected = '<span class="formwrapper">'.form_dropdown($name, $options, $selected, " class='chzn-select $class'")."</span>";
+			$selected = '<span class="formwrapper">'.form_dropdown($name, $options, $selected, "  class='chzn-select $class'")."</span>";
 			
 			return $selected;
 		}

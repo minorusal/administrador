3f5a89c92006dc0d5d5ba9f50034bfbd128d6dac
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-05-14 10:25:22 -0500

    Se actualiza modelo

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -31,13 +31,13 @@ class users_model extends Base_Model{
 						,U.activo
 						,C.user
 					FROM 
-						sys_usuarios U
-					left join sys_personales P on U.id_personal = P.id_personal
-					left join sys_claves     C on U.id_clave    = C.id_clave
-					left join sys_perfiles   N on U.id_perfil  = N.id_perfil
-					left join sys_paises     Pa on U.id_pais    = Pa.id_pais
-					left join sys_empresas   E on U.id_empresa  = E.id_empresa
-					left join sys_sucursales S on U.id_sucursal = S.id_sucursal
+						00_av_system.sys_usuarios U
+					left join 00_av_system.sys_personales P on U.id_personal = P.id_personal
+					left join 00_av_system.sys_claves     C on U.id_clave    = C.id_clave
+					left join 00_av_system.sys_perfiles   N on U.id_perfil  = N.id_perfil
+					left join 00_av_system.sys_paises     Pa on U.id_pais    = Pa.id_pais
+					left join 00_av_system.sys_empresas   E on U.id_empresa  = E.id_empresa
+					left join 00_av_system.sys_sucursales S on U.id_sucursal = S.id_sucursal
 					WHERE md5(C.user) = md5('$user') AND C.pwd = '$pwd' AND U.activo = 1
 					ORDER BY 
 						N.id_perfil;
@@ -53,6 +53,16 @@ class users_model extends Base_Model{
     * @return array
     */
 	function search_user_for_id($id_user){
+		// DB Info
+		$db1 	= $this->dbinfo[0]['db'];
+		$tbl1 	= $this->dbinfo[0]['tbl_usuarios'];
+		$tbl2 	= $this->dbinfo[0]['tbl_personales'];
+		$tbl3 	= $this->dbinfo[0]['tbl_claves'];
+		$tbl4 	= $this->dbinfo[0]['tbl_perfiles'];
+		$tbl5 	= $this->dbinfo[0]['tbl_paises'];
+		$tbl6 	= $this->dbinfo[0]['tbl_empresas'];
+		$tbl7 	= $this->dbinfo[0]['tbl_sucursales'];
+		// Query
 		$query  = "	SELECT 
 						U.id_usuario
 						,P.id_personal
@@ -77,13 +87,13 @@ class users_model extends Base_Model{
 						,U.activo
 						,C.user
 					FROM 
-						sys_usuarios U
-					left join sys_personales P on U.id_personal = P.id_personal
-					left join sys_claves     C on U.id_clave    = C.id_clave
-					left join sys_perfiles   N on U.id_perfil   = N.id_perfil
-					left join sys_paises     Pa on U.id_pais    = Pa.id_pais
-					left join sys_empresas   E on U.id_empresa  = E.id_empresa
-					left join sys_sucursales S on U.id_sucursal = S.id_sucursal
+						$db1.$tbl1 U
+					left join $db1.$tbl2 P on U.id_personal = P.id_personal
+					left join $db1.$tbl3 C on U.id_clave    = C.id_clave
+					left join $db1.$tbl4 N on U.id_perfil   = N.id_perfil
+					left join $db1.$tbl5 Pa on U.id_pais    = Pa.id_pais
+					left join $db1.$tbl6 E on U.id_empresa  = E.id_empresa
+					left join $db1.$tbl7 S on U.id_sucursal = S.id_sucursal
 					WHERE U.id_usuario = $id_user  AND U.activo = 1;
 				";
 		$query = $this->db->query($query);
@@ -97,15 +107,20 @@ class users_model extends Base_Model{
 	* @return array
 	*/
 	function search_modules_for_user($id_menu_n1 , $id_menu_n2, $id_menu_n3, $root = false ){
-
+		// DB Info
+		$db1 	= $this->dbinfo[0]['db'];
+		$tbl1 	= $this->dbinfo[0]['tbl_menu_n1'];
+		$tbl2 	= $this->dbinfo[0]['tbl_menu_n2'];
+		$tbl3 	= $this->dbinfo[0]['tbl_menu_n3'];
+		// Query
 		if($root){
 			$sys_navigate_n1 = "1";
-			$sys_navigate_n2 = "SELECT * FROM sys_menu_n2 WHERE activo = 1";
-			$sys_navigate_n3 = "SELECT * FROM sys_menu_n3 WHERE activo = 1";
+			$sys_navigate_n2 = "SELECT * FROM $db1.$tbl2 WHERE activo = 1";
+			$sys_navigate_n3 = "SELECT * FROM $db1.$tbl3 WHERE activo = 1";
 		}else{
 			$sys_navigate_n1 = "n1.id_menu_n1 IN ($id_menu_n1) AND n1.activo = 1";
-			$sys_navigate_n2 = "SELECT * FROM sys_menu_n2 WHERE id_menu_n2 IN ($id_menu_n2) AND activo = 1";
-			$sys_navigate_n3 = "SELECT * FROM sys_menu_n3 WHERE id_menu_n3 IN ($id_menu_n3) AND activo = 1";
+			$sys_navigate_n2 = "SELECT * FROM $db1.$tbl2 WHERE id_menu_n2 IN ($id_menu_n2) AND activo = 1";
+			$sys_navigate_n3 = "SELECT * FROM $db1.$tbl3 WHERE id_menu_n3 IN ($id_menu_n3) AND activo = 1";
 		}
 		$query = "	SELECT 
 						 n1.menu_n1
@@ -120,7 +135,7 @@ class users_model extends Base_Model{
 						,n3.routes as menu_n3_routes
 						,n3.icon as menu_n3_icon
 					FROM
-						sys_menu_n1 n1
+						$db1.$tbl1 n1
 					LEFT JOIN ($sys_navigate_n2 )  n2 ON n1.id_menu_n1 = n2.id_menu_n1
 					LEFT JOIN ($sys_navigate_n3)  n3 ON n3.id_menu_n2 = n2.id_menu_n2
 					WHERE

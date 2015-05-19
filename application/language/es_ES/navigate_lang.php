cc52857e383f28b156b840fc041a2dcc6ca3fe72
jmartinez <jorge.martinez@isolution.mx>
2015-05-19 17:17:58 -0500

    ajuste de vistas

--- application/language/es_ES/navigate_lang.php
+++ application/language/es_ES/navigate_lang.php
@@ -1,13 +1,13 @@
 <?php
-$lang['inventario']   = 'inventario';	
-$lang['catalogos']    = 'catalogos';
-$lang['articulos']    = 'articulos';	
-$lang['lineas']       = 'lineas';
+$lang['inventario']             = 'inventario';	
+$lang['catalogos']              = 'catalogos';
+$lang['articulos']              = 'articulos';	
+$lang['lineas']                 = 'lineas';
 $lang['unidad_de_medida']       = 'Unidad de medida';	
 
 $lang['catalogos_generales'] = 'Catalogos generales';
 $lang['control_de_usuarios'] = 'Control de usuarios';
-$lang['close_session'] = 'Cerrar Sesión';	
+$lang['close_session']       = 'Cerrar Sesión';	
 
 
 

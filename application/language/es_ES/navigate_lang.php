09989ab787326a260b4eea18b8d178781852340b
msalazar <daniel.salazar@isolution.mx>
2015-05-22 10:42:21 -0500

    carpeta de modelo d ebase de datos

--- application/language/es_ES/navigate_lang.php
+++ application/language/es_ES/navigate_lang.php
@@ -1,13 +1,46 @@
 <?php
 $lang['inventario']             = 'inventario';	
 $lang['catalogos']              = 'catalogos';
-$lang['articulos']              = 'articulos';	
+//MENU DE COMPRAS
+$lang['proveedores']            = 'proveedores';
+$lang['articulos']              = 'articulos';
+$lang['ordenes']                = 'ordenes';	
+$lang['catalogos']              = 'catalogos';
+$lang['presentaciones']         = 'presentaciones';
 $lang['lineas']                 = 'lineas';
+$lang['marcas']                 = 'marcas';
 $lang['unidad_de_medida']       = 'Unidad de medida';	
 
-$lang['formas_de_pago']      = 'Formas de pago';
-$lang['catalogos_generales'] = 'Catalogos generales';
-$lang['control_de_usuarios'] = 'Control de usuarios';
+//MENU DE ALMACEN
+$lang['entradas_recepcion']     = 'entradas recepción';
+$lang['salidas_despacho']       = 'salidas despacho';
+$lang['devoluciones']           = 'devoluciones';
+$lang['traspasos']              = 'traspasos';
+$lang['ajustes']                = 'ajustes';
+$lang['almacenes']              = 'almacenes';
+$lang['pasillos']               = 'pasillos';
+$lang['gavetas']                = 'gavetas';
+$lang['transportes']            = 'transportes';
+
+//MENU DE VENTAS
+$lang['clientes']               = 'clientes';
+$lang['vendedores']             = 'vendedores';
+$lang['facturas']               = 'facturas';
+$lang['ordenes']                = 'ordenes';
+
+//MENU DE ADMINISTRACION
+$lang['control_de_usuarios']    = 'Control de usuarios';
+$lang['usuarios']               = 'usuarios';
+$lang['puestos']                = 'puestos';
+$lang['areas']                  = 'areas';
+
+$lang['catalogos_generales']    = 'Catalogos generales';
+$lang['sucursales']             = 'sucursales';
+$lang['impuestos']              = 'impuestos';
+$lang['descuentos']            	= 'descuentos';
+$lang['formas_de_pago']         = 'Formas de pago';
+$lang['creditos']               = 'creditos';
+
 $lang['close_session']       = 'Cerrar Sesión';	
 
 ?>

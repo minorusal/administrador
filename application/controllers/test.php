34995aeba264aaad5cc926bf153a0353657804e2
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-05-26 16:07:15 -0500

    Se agregan pruebas de codigos qr

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -20,34 +20,50 @@ class test extends Base_Controller {
 		// Crea codigo de barras
 		$barras_txt = '0123456790123';
 		$this->barcode->barcode_img_tipo='png';
-		$barras = ($barrasImg = $this->barcode->create($barras_txt))?true:false; #Crea jpg
-		$barrasBmp = ($barras)?imagebmp('png',$barrasImg,'assets\tmp\barcode.bmp'):false; #Convierte jpg->bmp
-		// envia datos
+		$barras 	= ($barrasImg = $this->barcode->create($barras_txt))?true:false; #Crea jpg
+		$barrasBmp 	= ($barras)?imagebmp('png',$barrasImg,'assets\tmp\barcode.bmp'):false; #Convierte jpg->bmp
+		// Crea código QR
+		$qr_txt 	= 'http://www.isolution.mx';
+		$qr 		= ($qrImg = $this->codeqr->create($qr_txt))?true:false;
+		$qrBmp 		= ($qr)?imagebmp('png',$qrImg,'assets\tmp\qrcode.bmp'):false; #Convierte jpg->bmp
+		// Envía datos
 		$impData = array(
-			 'contenido' 	=> 'assets\tmp\ticket.txt'
-			,'logo' 		=> 'assets/images/logo.bmp'
-			// ,'impresora' 	=> 'PDFCreator'
-			,'formato' 		=> true
-			,'codebar' 		=> $barrasBmp #envia BMP
-			// ,'codeqr' 		=> 'assets/images/qr_isolution.bmp'			
+			 'contenido' 	=> 'assets\tmp\ticket.txt' #Contenido de ticket
+			,'logo' 		=> 'assets/images/logo.bmp' #Logo de empresa en BMP
+			// ,'impresora' 	=> 'PDFCreator' #Nombre de impresora local
+			,'formato' 		=> true #Formato de texto
+			,'codebar' 		=> $barrasBmp #Envia BMP
+			,'codeqr' 		=> $qrBmp #Envía BMP
 			);		
 		// Imprime ticket
 		if($this->impresion->enviar_a_impresora($impData)){
 			echo "Impresi&oacute;n enviada: ".date('Y-m-d H:i:s');
 		}
-		// Elimina imagenes
+		// Elimina imagenes generadas
 		if($barrasImg) unlink($barrasImg);
 		if($barrasBmp) unlink($barrasBmp);
+		if($qrImg) unlink($qrImg);
+		if($qrBmp) unlink($qrBmp);
 	}
 
 	public function barcode(){
 	// Prueba de generacion de código de barras
 		$this->barcode->barcode_img_tipo='jpg';
-		$this->barcode->create();
+		if($this->barcode->create()){
+			echo "Codigo de barras creado: ".date('Y-m-d H:i:s');
+		}else{
+			echo "No se creo";
+		}
 	}
 
-	public function qrcode(){
+	public function codeqr(){
 	// Prueba de generacion de código QR
+		$txt = "iSolution.mx";
+		if($this->codeqr->create($txt)){
+			echo "Codigo QR creado: ".date('Y-m-d H:i:s');
+		}else{
+			echo "No se creo";
+		}
 	}
 }
 ?>

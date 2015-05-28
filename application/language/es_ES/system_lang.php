f0adee8092343edc3b7297a36bd83e8d15a5c145
unknown <aaenciso@AencisoPC-MX.pae.cc>
2015-05-28 17:37:13 -0500

    se agregan mas campos en el listado, así como tambien la opcion de eliminar una orden

--- application/language/es_ES/system_lang.php
+++ application/language/es_ES/system_lang.php
@@ -32,6 +32,7 @@
 	$lang['msg_query_null']          = '<strong>Atencion!</strong><br>No se encontraron registros.';
 	$lang['msg_insert_success']      = '<strong>Exito!</strong><br>El registro de dio de alta correctamente';
 	$lang['msg_update_success']      = '<strong>Exito!</strong><br>Los cambios se guardaron correctamente';
+	$lang['msg_delete_success']      = '<strong>Exito!</strong><br>El registro se ha elimiminado correctamente';
 	$lang['msg_seesion_destroy']     = '<strong>Error!</strong><br>Su sesion ha finalizado por favor inicie sesion nuevamente, gracias.';
 	$lang['msg_campos_obligatorios'] = '<strong>Atencion!</strong><br>Los campos marcado con (*) son obligatorios, gracias';
 	$lang['msg_query_search']        = '<div style="float:left;"><p class="text-info">Se han encontrado %d registros para "%s".</p></div>'; 
@@ -40,6 +41,7 @@
 	$lang['btn_xlsx']     = 'Generar Excel';
 	$lang['btn_import_xlsx']     = 'Importar Excel';
 	$lang['btn_guardar']  = 'guardar cambios';
+	$lang['btn_eliminar']  = 'Eliminar registro';
 	$lang['btn_limpiar']  = 'limpiar';
 	$lang['btn_search']   = 'buscar';
 

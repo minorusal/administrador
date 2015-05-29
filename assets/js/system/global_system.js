5383eed3fb53354b5b86d9c619a76f414be2ba19
jmartinez <jorge.martinez@isolution.mx>
2015-05-29 10:29:54 -0500

    ajuste datepiocker

--- assets/js/system/global_system.js
+++ assets/js/system/global_system.js
@@ -34,7 +34,11 @@ function config_datepicker(){
         yearSuffix: ''
     };
     jQuery.datepicker.setDefaults(jQuery.datepicker.regional['es']);
- }
+}
+
+function datepicker(id){
+    jQuery('#'+id).datepicker();
+}
 function GoTop(){    
     //Check to see if the window is top if not then display button
     jQuery(window).scroll(function(){

ff78c18c404e91fb4ef5037c40cda6309b01e2f3
msalazar <daniel.salazar@isolution.mx>
2015-06-05 17:10:06 -0500

    avance catalogo usuarios

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -243,7 +243,32 @@ class users_model extends Base_Model{
       	$query = $this->db->query($query);
 		return $query->num_rows;
 	}
-
+	/*Inserta registro de usuarios*/
+		public function db_insert_data($data = array()){
+			print_debug($data);
+			// DB Info		
+			$tbl = $this->tbl;
+			// Query
+			$insert_personal = $this->insert_item($tbl['usuarios'], $data);
+			$array_clave = array(
+				 'user'     => ''
+				,'pwd'      => ''
+				,'registro' => ''
+				);
+			$insert_clave = $this->insert_item($tbl['claves'], $data);
+			$array_usuarios = array(
+				'id_personal' => $this->db->insert_id($insert)
+				,'id_clave'   => $this->db->insert_id($insert_clave)
+				,'id_perfil'  => $data['id_perfil']
+				,'id_pais'    => $data
+				);
+			return $insert;
+			if($insert){
+				return $insert;
+			}else{
+				return false;
+			}
+	}
 	
 
 }

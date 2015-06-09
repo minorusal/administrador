d27a1e0c99779102f7b14b42374a63daf96c2235
msalazar <daniel.salazar@isolution.mx>
2015-06-09 10:23:38 -0500

    avance

--- application/models/nutricion/valores_nutricionales_model.php
+++ application/models/nutricion/valores_nutricionales_model.php
@@ -4,10 +4,11 @@ class valores_nutricionales_model extends Base_Model{
 	public function get_valores_nutricionales_unico($id_articulo){
 		// DB Info
 		$tbl = $this->tbl;
+		//print_debug($tbl['nutricion_valores_nutricionales']);
 		$condicion = array('id_compras_articulos =' => $id_articulo); 
-		$existe    = $this->row_exist($tbl['nutricion_valores_nutricionales'], $condicion,true);
+		$existe    = $this->row_exist($tbl['nutricion_valores_nutricionales'], $condicion);
 		if($existe){
-			print_debug($existe);
+			
 		}
 		else
 		{

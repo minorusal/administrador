22ac17aa8d26a3cec5ef601403143901bb198ce1
msalazar <daniel.salazar@isolution.mx>
2015-06-09 15:08:02 -0500

    avance valores

--- application/language/es_ES/system_lang.php
+++ application/language/es_ES/system_lang.php
@@ -35,7 +35,7 @@
 	$lang['msg_delete_success']      		   = '<strong>Exito!</strong><br>El registro se ha elimiminado correctamente';
 	$lang['msg_seesion_destroy']     		   = '<strong>Error!</strong><br>Su sesion ha finalizado por favor inicie sesion nuevamente, gracias.';
 	$lang['msg_campos_obligatorios']           = '<strong>Atencion!</strong><br>Los campos marcado con (*) son obligatorios, gracias';
-	$lang['msg_campos_numericos']              = '<strong>Atencion!</strong><br>Los campos marcado con (*) son de tipo numerico, gracias';
+	$lang['msg_campos_numericos']              = '<strong>Atencion!</strong><br>Los campos deben contener numeros, gracias';
 	$lang['msg_query_search']        		   = '<div style="float:left;"><p class="text-info">Se han encontrado %d registros para "%s".</p></div>'; 
 	$lang['msg_err_clv']             		   = '<strong>Advertencia!</strong><br>La clave asignada ya se ha proporcionado a otro registro, porfavor intente con una clave diferente';
 

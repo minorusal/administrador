c86c53c178b7dab767e1877c71c1c3600141c383
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-06-22 12:27:24 -0500

    Se trabaja en formato de la ordend e compra

--- application/language/es_ES/compras/ordenes_lang.php
+++ application/language/es_ES/compras/ordenes_lang.php
@@ -11,7 +11,7 @@ $lang['proveedor'] 			= "Proveedor";
 $lang['descripcion'] 		= "Descripción";
 $lang['sucursal'] 			= "Sucursal";
 $lang['acciones'] 			= "Acciones";
-$lang['lst_articulos_label']= "Seleccione un articulo:";
+$lang['lst_articulos_label']= "Busque y seleccione un articulo: ";
 
 
 $lang['fecha_registro'] 	= "Creado el";
@@ -23,12 +23,21 @@ $lang['entrega_direccion'] 	= "Dirección de entrega";
 $lang['entrega_fecha'] 	    = "Fecha de entrega";
 $lang['prefactura_num'] 	= "No. prefactura";
 $lang['observaciones'] 	    = "Observaciones";
-$lang['creditos'] 	    	= "Creditos";
+$lang['credito'] 	    	= "Crédito";
 $lang['forma_pago'] 	    = "Forma de pago";
 $lang['orden_tipo'] 	    = "Tipo de orden";
 $lang['agregar_articulos']  = "Agregar Artículos";
 $lang['clave_corta']  		= "Clave corta";
-$lang['valor_imp']  		= "valor/imp";
-$lang['costo_cantidad']  	= "costo * cantidad";
+$lang['imp']  				= "% Imp.";
+$lang['valor_imp']  		= "Impuesto";
+$lang['costo_cantidad']  	= "Costo x Cantidad";
+$lang['costo_unitario']  	= "Costo unitario";
+$lang['accion']  			= "Acción";
 
+$lang['subtotal']  			= "Subtotal";
+$lang['impuesto']  			= "Impuesto";
+$lang['descuento']  		= "Descuento";
+$lang['a_pagar']  			= "Total a Pagar";
+$lang['cerrar_orden'] 		= "Cerrar Orden";
+$lang['cancelar_orden']  	= "Cancelar Orden";
 ?>

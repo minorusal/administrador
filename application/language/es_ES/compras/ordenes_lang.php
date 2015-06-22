1a568e772f0b5a60b5a4b5d0c432472d9180c656
alejandro enciso <alvaro.enciso@isolution.mx>
2015-06-22 12:36:46 -0500

    incorporacion de cambios con oscar en ordenes

--- application/language/es_ES/compras/ordenes_lang.php
+++ application/language/es_ES/compras/ordenes_lang.php
@@ -10,13 +10,7 @@ $lang['proveedor'] 			= "Proveedor";
 $lang['descripcion'] 		= "Descripción";
 $lang['sucursal'] 			= "Sucursal";
 $lang['acciones'] 			= "Acciones";
-<<<<<<< HEAD
-$lang['lst_articulos_label']= "Seleccione un articulo:";
-=======
 $lang['lst_articulos_label']= "Busque y seleccione un articulo: ";
-
-
->>>>>>> c86c53c178b7dab767e1877c71c1c3600141c383
 $lang['fecha_registro'] 	= "Creado el";
 $lang['registro_por'] 		= "Creado por";
 $lang['msg_err_clv']     	= '<strong>Advertencia!</strong><br>La clave asignada ya se ha proporcionado a otro registro, por favor intente con una clave diferente.';
@@ -30,21 +24,15 @@ $lang['forma_pago'] 	    = "Forma de pago";
 $lang['orden_tipo'] 	    = "Tipo de orden";
 $lang['agregar_articulos']  = "Agregar Artículos";
 $lang['clave_corta']  		= "Clave corta";
-<<<<<<< HEAD
-$lang['valor_imp']  		= "valor/imp";
-$lang['costo_cantidad']  	= "costo * cantidad";
-=======
 $lang['imp']  				= "% Imp.";
 $lang['valor_imp']  		= "Impuesto";
 $lang['costo_cantidad']  	= "Costo x Cantidad";
 $lang['costo_unitario']  	= "Costo unitario";
 $lang['accion']  			= "Acción";
-
 $lang['subtotal']  			= "Subtotal";
 $lang['impuesto']  			= "Impuesto";
 $lang['descuento']  		= "Descuento";
 $lang['a_pagar']  			= "Total a Pagar";
 $lang['cerrar_orden'] 		= "Cerrar Orden";
 $lang['cancelar_orden']  	= "Cancelar Orden";
->>>>>>> c86c53c178b7dab767e1877c71c1c3600141c383
 ?>

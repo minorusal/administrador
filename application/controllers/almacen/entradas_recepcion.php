3a0c4a8cb0666d276c96a0b0d72b907ad5ea862c
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-07-01 17:28:46 -0500

    Se optimiza uso de modal

--- application/controllers/almacen/entradas_recepcion.php
+++ application/controllers/almacen/entradas_recepcion.php
@@ -509,14 +509,11 @@ class entradas_recepcion extends Base_Controller{
 		}
 		echo json_encode($json_respuesta);
 	}
-	public function modal_lote_caducidad(){
-		$titulo_modal = $this->lang_item("modal_titulo",true);
+	public function modal_lote_caducidad(){		
 		$id = $this->ajax_post('id');
 		$caducidad = $this->ajax_post('caducidad_val');
 		$lote = $this->ajax_post('lote_val');
-		$u_m = $this->ajax_post('u_m_val');		
-		$accion 	= $this->tab['modal'];
-		$url_link 	= $this->modulo.'/'.$this->seccion.'/'.$this->submodulo.'/'.$accion;
+		$u_m = $this->ajax_post('u_m_val');				
 		// template html modal
 		$tabData_modal = array('id'=>$id, 'caducidad'=>$caducidad, 'lote'=>$lote, 'u_m'=>$u_m);
 		$tabData_modal['lbl_lote'] 			= $this->lang_item("lote",true);
@@ -525,12 +522,13 @@ class entradas_recepcion extends Base_Controller{
 		$url_modal_tpl = $this->modulo.'/'.$this->seccion.'/'.$this->submodulo.'/'.'modal_lote_tpl';
 		$html = $this->load_view_unique($url_modal_tpl ,$tabData_modal, true);
 		// Cargar modal
+		$titulo_modal = $this->lang_item("modal_titulo",true);
 		$arg_body   = array(
 							'header'=>array('id'=> 1,'html'=>$titulo_modal),
 							'body' =>array('id'=> 'test','html'=>$html), 
 							'footer' => array('id'=> 'test','html'=>''));
-		$tabData['modal'] = toggle_modal_tpl('lote', $arg_body);
-		echo json_encode( $this->load_view_unique($url_link ,$tabData, true));
+		$html_modal = toggle_modal_tpl('lote', $arg_body);
+		echo json_encode($html_modal);
 	}
 }
 ?>

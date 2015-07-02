0b1be0bd15293832b0749b355551a1ac05706708
alejandro enciso <alvaro.enciso@isolution.mx>
2015-07-02 13:12:15 -0500

    se agraga el diccionario en entradas

--- application/language/es_ES/almacen/entradas_recepcion_lang.php
+++ application/language/es_ES/almacen/entradas_recepcion_lang.php
@@ -5,5 +5,19 @@
 	$lang['caducidad']     			= 'caducidad';
 	$lang['um']     				= 'unidad de medida';
 	$lang['modal_titulo']     		= 'Asignación de lote y caducidad';
-	
+	$lang['orden_num']     			= 'Número de la orden';
+	$lang['entrega_fecha']         	= 'fecha de la entrega';
+	$lang['forma_pago']         	= 'Forma de pago';	
+	$lang['no_factura']         	= 'No. de la factura';	
+	$lang['fecha_factura']         	= 'fecha de la factura';	
+	$lang['#'] 						= '#';	
+	$lang['costo_unitario']         = 'Costo unitario';	
+	$lang['costo_cantidad']        	= 'costo*cantidad';	
+	$lang['valor_imp']         		= 'valor del imp.';	
+	$lang['aceptar']         		= 'Aceptar listado';
+	$lang['comentarios_entrada']    = 'Comentarios de entrada';
+	$lang['recibir_enetrada']       = 'Recibir entrada';
+	$lang['rechazar_entrada']       = 'Rechazar entrada';
+	$lang['recibir_lote']           = 'aceptar';
+	$lang['devolver_lote']          = 'cancelar';
 ?>

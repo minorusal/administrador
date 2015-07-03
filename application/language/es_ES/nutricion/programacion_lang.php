7ef66b767510c935c7e9c949bcfdbdb9e3bf0239
jmartinez <jorge.martinez@isolution.mx>
2015-07-03 11:08:14 -0500

    se crea vista de progrmacion

--- application/language/es_ES/nutricion/programacion_lang.php
+++ application/language/es_ES/nutricion/programacion_lang.php
@@ -1,6 +1,11 @@
 <?php
 	$lang['lbl_config_programacion']         = 'Configuración de Programacion';
-	$lang['lbl_input_fecha_inicio'] = 'Fecha de Inicio de Operación:';
-	$lang['lbl_input_fecha_temrino'] = 'Fecha de termino de Operación:';
-	$lang['lbl_dias_descartados'] ='Dias Descartados y Festivos';
+	$lang['lbl_input_fecha_inicio']          = 'Fecha de Inicio de Operación:';
+	$lang['lbl_input_fecha_temrino']         = 'Fecha de termino de Operación:';
+	$lang['lbl_dias_descartados']            ='Dias Descartados y Festivos';
+	$lang['lbl_info_dias_descartados']       ='Selección de Días para Descartar en la Programación:';
+	$lang['lbl_agregar_menus']               = 'Agregar Menus';
+	$lang['lbl_info_agregar_menus'] = 'Agrega los menus que seran ciclados en el calendario de Programación';
+	$lang['lbl_orden_menus'] = 'Orden de Menus';
+	$lang['lbl_info_orden_menus'] = 'Coloca los menus en el orden que apareceran en el ciclo, para moverlos solo basta dar click sobre un menu y desplazalo al lugar que le corresponda';
 ?>

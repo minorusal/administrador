20b50fb0efc0de96ef466ddb5c7284604e9e5023
jmartinez <jorge.martinez@isolution.mx>
2015-07-03 10:57:08 -0500

    configuracion de idioma seccion priogramacion

--- application/language/es_ES/nutricion/programacion_lang.php
+++ application/language/es_ES/nutricion/programacion_lang.php
@@ -1,3 +1,6 @@
 <?php
-
+	$lang['lbl_config_programacion']         = 'Configuración de Programacion';
+	$lang['lbl_input_fecha_inicio'] = 'Fecha de Inicio de Operación:';
+	$lang['lbl_input_fecha_temrino'] = 'Fecha de termino de Operación:';
+	$lang['lbl_dias_descartados'] ='Dias Descartados y Festivos';
 ?>

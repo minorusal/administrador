50371fd22a09bbbfaca1cfa92320b89bf2df9c6d
msalazar <daniel.salazar@isolution.mx>
2015-07-08 17:50:22 -0500

    nuevos mensajes

--- application/language/es_ES/administracion/formas_de_pago_lang.php
+++ application/language/es_ES/administracion/formas_de_pago_lang.php
@@ -5,14 +5,14 @@ $lang['agregar'] 			 = "Nueva forma de pago";
 $lang['listado'] 			 = "Listado de formas de pago";
 $lang['detalle'] 			 = "Detalle";
 
-$lang['lbl_forma_pago'] = 'forma de pago';
-$lang['lbl_fecha_registro'] = 'fecha de registro';
-$lang['lbl_usuario_registro'] = 'registrado por';
+$lang['lbl_forma_pago']          = 'forma de pago';
+$lang['lbl_fecha_registro']      = 'fecha de registro';
+$lang['lbl_usuario_registro']    = 'registrado por';
 $lang['lbl_ultima_modificacion'] = 'ultima modificación';
 
-$lang['nombre_forma_pago']   = "forma de pago";
-$lang['clave_corta'] 		 = "clave corta";
-$lang['descripcion']         = "descripción";
+$lang['lbl_forma_pago']   = "forma de pago";
+$lang['lbl_clave_corta']  = "clave corta";
+$lang['lbl_descripcion']  = "descripción";
 
 $lang['fecha_registro'] 	= "Creado el";
 $lang['registro_por'] 		= "Creado por";

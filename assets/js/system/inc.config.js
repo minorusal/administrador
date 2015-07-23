4ffae753ade3e3cf4fe6f7478a65ab8ff4c1165a
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-07-23 10:58:54 -0500

    Se corrige archivo de config js

--- assets/js/system/inc.config.js
+++ assets/js/system/inc.config.js
@@ -1,9 +1,27 @@
 function path(Folder){
 // Obtiene Carpeta raiz
-    if(!Folder){Folder='adminventas_ci';}
-    Folrder = Folder + '/';
+    if(!Folder){Folder='admincontrol';}
+    Folder = Folder + '/';
     var dominio = document.domain;
     var raiz = window.location.pathname.split(Folder);
-    var ruta = raiz[0] + Folder + '/';
+    var ruta = raiz[0] + Folder;
     return ruta;
 }
+
+// function path(Folder){
+// // Lee un JSON
+//     var json_url = "assets/js/system/config.json";
+//     var valor = '';
+//     jQuery.getJSON(json_url, function(data) {       
+//         valor = data['system_path'];
+//     });
+//     setTimeout(function(){
+//         var system_path = valor;
+//         Folder = system_path;
+//     }, 5);
+//     Folrder = Folder + '/';
+//     var dominio = document.domain;
+//     var raiz = window.location.pathname.split(Folder);
+//     var ruta = raiz[0] + Folder + '/';
+//     return ruta;
+// }
